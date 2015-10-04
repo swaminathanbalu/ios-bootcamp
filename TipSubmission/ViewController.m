afebffe8ded7d64336b51a403959131f03e4f7cb
Swaminathan Balu <sbalu@groupon.com>
2015-10-04 09:34:44 -0700

    Initial Commit

---
+++ TipSubmission/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TipSubmission
+//
+//  Created by Swaminathan Balu on 10/4/15.
+//  Copyright (c) 2015 Swaminathan Balu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ TipSubmission/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TipSubmission
+//
+//  Created by Swaminathan Balu on 10/4/15.
+//  Copyright (c) 2015 Swaminathan Balu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
